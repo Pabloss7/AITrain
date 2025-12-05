dceec295b54e877f697676feafd57e55dbc856b3
Pabloss7 <spablo571@gmail.com>
2025-12-05 13:24:59 +0100

    ADD basic xgboost model

--- AI/src/main.py
+++ AI/src/main.py
@@ -2,8 +2,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import warnings
-from sklearn.model_selection import train_test_split
 import xgboost as xgb
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import OrdinalEncoder
 
 
 warnings.filterwarnings("ignore")
@@ -12,9 +14,10 @@ matches = pd.read_parquet("../data/matches_clean_dataset.parquet")
 
 # We split the result of the game so we want to predict who will win
 x, y = matches.drop(columns=["win"]), matches["win"]
-
+y_encoded =  OrdinalEncoder().fit_transform(y)
 #Split data into train and test
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x, y_encoded, random_state=1,stratify=y_encoded)
+
 
 dtrain_reg = xgb.DMatrix(x_train, y_train, enable_categorical=True)
 dtest_reg = xgb.DMatrix(x_test, y_test, enable_categorical=True)
@@ -23,12 +26,30 @@ dtest_reg = xgb.DMatrix(x_test, y_test, enable_categorical=True)
 
 # Define hyperparameters
 params = {"objective": "reg:squarederror", "tree_method": "gpu_hist"}
-
 # Number of boosting rounds
-n = 100
+n = 1000
+
+evaluations = [(dtrain_reg, "train"), (dtest_reg, "validation")]
+
 model = xgb.train(
    params=params,
    dtrain=dtrain_reg,
    num_boost_round=n,
+   evals=evaluations,
+   verbose_eval=50,
+   early_stopping_rounds=50
 )
-print(matches.dtypes)
+
+preds = model.predict(dtest_reg)
+
+rmse = mean_squared_error(y_test, preds, squared=False)
+print(f"RMSE of the base model: {rmse:.3f}")
+results = xgb.cv(
+   params,dtrain_reg,
+   num_boost_round=n,
+   nfold=5,
+   early_stopping_rounds=20
+)
+print(results.head())
+best_rmse = results['test-rmse-mean'].min()
+print(f"Best RMSE: {best_rmse:.3f}")
