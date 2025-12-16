7d0924e17ce53703a522392d7272f8b5a23872a4
Pabloss7 <spablo571@gmail.com>
2025-12-16 13:03:59 +0100

    FIX shap explainer structure

--- AI/src/shap_explainer.py
+++ AI/src/shap_explainer.py
@@ -3,33 +3,60 @@ import shap
 import xgboost as xgb
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
-matches = pd.read_parquet("./data/matches_clean_dataset.parquet")
-categorical_columns = ["championName", "individualPosition"]
+explainer = None
+feature_columns = None
 
-matches_encoded = pd.get_dummies(matches, columns=categorical_columns)
+def base_path():
+    return os.path.dirname(__file__)
 
-X = matches_encoded.drop(columns=["win"])
+def load_matches():
+    path = os.path.join(base_path(), "data", "matches_clean_dataset.parquet")
+    return pd.read_parquet(path)
 
-background = shap.sample(X, 100)
+def load_tools():
+    global explainer, feature_columns
 
-model = xgb.Booster()
-model.load_model("../models/xgboost_model.json")
+    if explainer is not None:
+        return explainer, feature_columns
+    
+    matches = load_matches()
+    categorical_columns = ["championName", "individualPosition"]
 
-explainer = shap.TreeExplainer(
-    model,
-    data=background,
-    feature_perturbation="interventional"
-    )
+    matches_encoded = pd.get_dummies(matches, columns=categorical_columns)
+
+    X = matches_encoded.drop(columns=["win"])
+
+    background = shap.sample(X, 100)
+
+    model_path = os.path.join(base_path(), "models", "xgboost_model.json")
+
+    model = xgb.Booster()
+    model.load_model(model_path)
+
+    explainer = shap.TreeExplainer(
+        model,
+        data=background,
+        feature_perturbation="interventional"
+        )
+    return explainer, feature_columns
+
 #TODO: ARREGLAR EL PROBLEMA DE LAS COLUMNAS DE SHAP
 def explain_match(single_match_features):
-    shap_values = explainer.shap_values(single_match_features)
-    print("i'm here")
+    explainer, feature_columns = load_explainer()
+
+    shap_values = explainer.shap_values(single_match_features)[0]
+    
     sorted_idx = np.argsort(np.abs(shap_values))[::-1][:5]
 
     top_features = [
-    (X.columns[i], single_match_features[i], shap_values[i])
-    for i in sorted_idx
+        (
+            feature_columns[i],
+            single_match_features.iloc[0, i],
+            shap_values[i]
+        )
+        for i in sorted_idx
     ]
     
     return top_features
